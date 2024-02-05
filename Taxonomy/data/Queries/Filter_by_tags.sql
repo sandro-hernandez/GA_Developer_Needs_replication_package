bbd02394366af1c8edfb1117918c798ee13d9a37
sandrohg10 <69373226+sandrohg10@users.noreply.github.com>
2024-02-05 12:57:58 +0100

    create sample

--- Taxonomy/data/Queries/Filter_by_tags.sql
+++ Taxonomy/data/Queries/Filter_by_tags.sql
@@ -4,15 +4,15 @@ SELECT DISTINCT
 FROM Posts p
 WHERE 
     (
-        p.Tags LIKE '%<github-actions>%' OR
-        p.Tags LIKE '%<building-github-actions>%' OR
-        p.Tags LIKE '%<github-actions-self-hosted-runners>%' OR
-        p.Tags LIKE '%<github-actions-runners>%' OR
-        p.Tags LIKE '%<github-actions-services>%' OR
-        p.Tags LIKE '%<github-actions-artifacts>%' OR
-        p.Tags LIKE '%<github-actions-reusable-workflows>%' OR
-        p.Tags LIKE '%<github-actions-workflows>%' OR
-        p.Tags LIKE '%<github-actions-marketplace>%'
+        p.tags LIKE '%<github-actions>%' OR
+        p.tags LIKE '%<building-github-actions>%' OR
+        p.tags LIKE '%<github-actions-self-hosted-runners>%' OR
+        p.tags LIKE '%<github-actions-runners>%' OR
+        p.tags LIKE '%<github-actions-services>%' OR
+        p.tags LIKE '%<github-actions-artifacts>%' OR
+        p.tags LIKE '%<github-actions-reusable-workflows>%' OR
+        p.tags LIKE '%<github-actions-workflows>%' OR
+        p.tags LIKE '%<github-actions-marketplace>%'
     )
-    AND p.CreationDate >= '2019-01-01 00:00:00'
-    AND p.CreationDate < '2023-12-31 23:59:59'
+    AND p.creation_date >= '2019-01-01 00:00:00'
+    AND p.creation_date < '2023-12-31 23:59:59';
